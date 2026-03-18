e9863b858fd728651f71f8d3aedf2cb9be7bac2f
Azril Fatfa <azril.fatfa@gmail.com>
2026-03-18 11:58:15 +0800

    feat: fixing spageti code

--- frontend/cCodeBackendchat-realtimefrontendsrcpagesFriendsPage.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react'; export default function FriendsPage() { return <div>Friends</div>; }
--- frontend/cCodeBackendchat-realtimefrontendsrcpagesRoomManagementPage.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react'; export default function RoomManagementPage() { return <div></div>; }
